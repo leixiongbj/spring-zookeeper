469aaa37640b6d8ba5ffd6810ad286809bc41c11
xionglei3 <xionglei3@jd.com>
2018-11-30 17:15:43 +0800

    初始化

---
+++ src/main/java/cn/king/springzookeeper/SpringZookeeperApplication.java
@@ -0,0 +1,15 @@
+package cn.king.springzookeeper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author king
+ */
+@SpringBootApplication
+public class SpringZookeeperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringZookeeperApplication.class, args);
+	}
+}
